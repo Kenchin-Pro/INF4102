3bbb7875dcb0eb1a88c56e366ae334299cc0dfcb
Kenchin_ <106256182+Kenchin-Pro@users.noreply.github.com>
2022-10-05 09:51:09 +0200

    Update find_motive.cpp

--- find_motive/find_motive.cpp
+++ find_motive/find_motive.cpp
@@ -34,6 +34,6 @@ int main(int argc, char **argv)
 		cout << "The file " << file_path << " could not be opened." << endl;
 	}
 	Fichier_ouvert.close();
-	cout << "The file " << file_path << " contains " << nombre_de_motif << " words containing the motive " << motive << "." << endl;
+	cout << "The file " << file_path << " contains " << nombre_de_motif << " words containing the motive " << motive << endl;
 	return 0;
 }
